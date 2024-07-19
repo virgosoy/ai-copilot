7502a087cd711b6ccfd6fea230726459cbf1d45f
virgosoy <virgosoy@126.com>
2024-07-19 10:03:57 +0800

    feat: 去掉大部分 runnable api，加快 LangServe 启动速度

--- backend/langserve/app/server.py
+++ backend/langserve/app/server.py
@@ -24,75 +24,75 @@ async def redirect_root_to_docs():
 # from rag_chroma_multi_modal import chain as rag_chroma_multi_modal_chain
 # add_routes(app, rag_chroma_multi_modal_chain, path="/rag-chroma-multi-modal")
 
-from chain_of_note_wiki import chain as chain_of_note_wiki_chain
-add_routes(app, chain_of_note_wiki_chain, path="/chain-of-note-wiki")
+# from chain_of_note_wiki import chain as chain_of_note_wiki_chain
+# add_routes(app, chain_of_note_wiki_chain, path="/chain-of-note-wiki")
 
 # endregion
 
 # region 添加路由
-from .chains.demo.hello_world.chain import chain
-add_routes(app, chain, path="/hello-world")
+# from .chains.demo.hello_world.chain import chain
+# add_routes(app, chain, path="/hello-world")
 
-from .chains.demo.rag.word import chain
-add_routes(app, chain, path="/word")
+# from .chains.demo.rag.word import chain
+# add_routes(app, chain, path="/word")
 
-from .chains.demo.rag import chain
-add_routes(app, chain, path="/rag")
+# from .chains.demo.rag import chain
+# add_routes(app, chain, path="/rag")
 
-from .chains.demo.rag.xiaofeizhequanyi_demo import chain
-add_routes(app, chain, path="/xiaofeizhequanyi")
+# from .chains.demo.rag.xiaofeizhequanyi_demo import chain
+# add_routes(app, chain, path="/xiaofeizhequanyi")
 
-from .chains.demo.rag.pdf import chain
-add_routes(app, chain, path="/pdf")
+# from .chains.demo.rag.pdf import chain
+# add_routes(app, chain, path="/pdf")
 
-from .chains.demo.rag.pdf_and_spacy import chain
-add_routes(app, chain, path="/pdf-and-spacy")
+# from .chains.demo.rag.pdf_and_spacy import chain
+# add_routes(app, chain, path="/pdf-and-spacy")
 
-from .chains.demo.rag.pdf_and_spacy_multi_query import chain
-add_routes(app, chain, path="/pdf-and-spacy-multi-query")
+# from .chains.demo.rag.pdf_and_spacy_multi_query import chain
+# add_routes(app, chain, path="/pdf-and-spacy-multi-query")
 
-from .chains.demo.load_document.file_processing import chain
-add_routes(
-    app,
-    chain,
-    config_keys=["configurable"],
-    path="/pdf-file-processing",
-)
+# from .chains.demo.load_document.file_processing import chain
+# add_routes(
+#     app,
+#     chain,
+#     config_keys=["configurable"],
+#     path="/pdf-file-processing",
+# )
 
-from .chains.demo.chat_history.tuples import chain1, chain2, chain3
-add_routes(
-    app,
-    chain1,
-    config_keys=["configurable"],
-    path="/chat-history-tuples-1"
-)
-add_routes(
-    app,
-    chain2,
-    config_keys=["configurable"],
-    path="/chat-history-tuples-2"
-)
-add_routes(
-    app,
-    chain3,
-    config_keys=["configurable"],
-    path="/chat-history-tuples-3"
-)
+# from .chains.demo.chat_history.tuples import chain1, chain2, chain3
+# add_routes(
+#     app,
+#     chain1,
+#     config_keys=["configurable"],
+#     path="/chat-history-tuples-1"
+# )
+# add_routes(
+#     app,
+#     chain2,
+#     config_keys=["configurable"],
+#     path="/chat-history-tuples-2"
+# )
+# add_routes(
+#     app,
+#     chain3,
+#     config_keys=["configurable"],
+#     path="/chat-history-tuples-3"
+# )
 
-from .chains.vision.vision import chain,chain2,chain3
-add_routes(app, chain, path="/vision")
-add_routes(app, chain2, path="/vision-2")
-add_routes(app, chain3, path="/vision-3")
+# from .chains.vision.vision import chain,chain2,chain3
+# add_routes(app, chain, path="/vision")
+# add_routes(app, chain2, path="/vision-2")
+# add_routes(app, chain3, path="/vision-3")
 
-from .chains.demo.hello_world.chat_playground import chain
-add_routes(
-    app,
-    chain,
-    path="/chat-playground",
-    enable_feedback_endpoint=True,
-    enable_public_trace_link_endpoint=True,
-    playground_type="chat",
-)
+# from .chains.demo.hello_world.chat_playground import chain
+# add_routes(
+#     app,
+#     chain,
+#     path="/chat-playground",
+#     enable_feedback_endpoint=True,
+#     enable_public_trace_link_endpoint=True,
+#     playground_type="chat",
+# )
 
 from .chains.chat.chat_and_vision import chain1, chain2, chain3, chain4, chain5
 add_routes(app, chain1, path="/chat-and-vision-1")
@@ -107,26 +107,26 @@ add_routes(app, chain_test_2, path="/local-knowledge-rag-test-2")
 add_routes(app, chain_vision, path="/local-knowledge-rag-vision")
 add_routes(app, chain_chat_and_vision, path="/local-knowledge-rag-chat-and-vision")
 
-from .chains.demo.configure import chain, chain1, chain2
-add_routes(app, chain, path="/configure")
-add_routes(app, chain1, path="/configure1")
-add_routes(app, chain2, path="/configure2")
+# from .chains.demo.configure import chain, chain1, chain2
+# add_routes(app, chain, path="/configure")
+# add_routes(app, chain1, path="/configure1")
+# add_routes(app, chain2, path="/configure2")
 
 # endregion
 
 # region 纯路由
-from .chains.demo.load_document import save_files
+# from .chains.demo.load_document import save_files
 
-@app.post('/upload-files')
-async def upload_files(files: list[UploadFile]):
-    result = await save_files(files)
-    return result
+# @app.post('/upload-files')
+# async def upload_files(files: list[UploadFile]):
+#     result = await save_files(files)
+#     return result
 
-from .chains.demo.load_document import load_local_pdf_and_spacy
-@app.get('/load-local-file')
-async def load_local_file():
-    await load_local_pdf_and_spacy()
-    return "success"
+# from .chains.demo.load_document import load_local_pdf_and_spacy
+# @app.get('/load-local-file')
+# async def load_local_file():
+#     await load_local_pdf_and_spacy()
+#     return "success"
 
 # endregion
 
