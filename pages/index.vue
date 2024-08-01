5f14b4cda91501baf05652ec17350f77b40a4005
virgosoy <virgosoy@126.com>
2024-08-01 16:07:53 +0800

    feat: 调整首页链接

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,10 @@
   <div class="flex flex-col">
     <ULink to="/chat/chat-and-vision"
       class="text-primary">Chat And Vision</ULink>
+    <ULink to="/rag/local-knowledge-rag-local-text-chat"
+      class="text-primary">Local Knowledge Rag | Only Text Chat And Use Local Embedding</ULink>
     <ULink to="/rag/local-knowledge-rag-text-chat"
-      class="text-primary">Local Knowledge Rag | Only Text Chat</ULink>
+      class="text-primary">Local Knowledge Rag | Only Text Chat And Use Openai Embedding</ULink>
     <ULink to="/rag/local-knowledge-rag-chat-and-vision"
       class="text-primary">Local Knowledge Rag | Chat And Vision（@Deprecated）</ULink>
   </div>
