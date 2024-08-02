91f4ae494a1ce82a029d1f31fa9385a9f6864cbd
virgosoy <virgosoy@126.com>
2024-08-02 15:27:51 +0800

    feat: 新增部分模型供应商 anthropic 的模型

--- components/biz/BizConfigModelSelect.vue
+++ components/biz/BizConfigModelSelect.vue
@@ -75,7 +75,12 @@ const modelProvider = computed({
 
 const modelProviderMapModel : Record<typeof modelProviderOptions[number], string[]> = {
   openai: ['gpt-4-turbo', 'gpt-4o', 'gpt-3.5-turbo'],
-  anthropic: ['claude-3-haiku-20240307'],
+  anthropic: [
+    'claude-3-5-sonnet-20240620',
+    'claude-3-opus-20240229',
+    // 'claude-3-sonnet-20240229',
+    'claude-3-haiku-20240307',
+  ],
   ollama: ['qwen2', 'qwen2:72B', 'llama3'],
 }
 const modelOptions = computed(() => {
